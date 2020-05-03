9ec5d405525eed3ac27a251441ebdf425215f681
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2020-05-03 04:45:58 -0300

    voy tratando de cerrar clase 1

---
+++ clases/1_clase/ciaa/psf1/visualize.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from   matplotlib.animation import FuncAnimation
+import os
+
+length = 512
+fs     = 1000
+header = b'header'
+fig    = plt.figure()
+adcAxe = fig.add_subplot ( 1,1,1 )
+adcLn, = plt.plot ( [],[],'r-o'   )
+adcAxe.grid       ( True          )
+adcAxe.set_ylim   ( -0.5 ,0.5     )
+adcAxe.set_xlim   ( 0 , length/fs )
+
+def initFiles():
+    global logFile
+    logFile   = open("log.bin","rb")
+    logFile.seek(0, os.SEEK_END)
+
+def findHeader(f):
+    index  = 0
+    sync   = False
+    while sync==False:
+        data=b''
+        while len(data) <1:
+            data = f.read(1)
+        if data[0]==header[index]:
+            index+=1
+            if index>=len(header):
+                sync=True
+        else:
+            index=0
+
+def readInt4File(f):
+    raw=b''
+    while len(raw)<2:
+        raw   += f.read(1)
+    return (int.from_bytes(raw[0:2],"little",signed=True))
+
+def update(t):
+    findHeader ( logFile )
+    adc = []
+    for chunk in range(length):
+        adc.append (readInt4File(logFile)/(2**15))
+    time = np.linspace(0,length/fs,length)
+    adcLn.set_data ( time,adc )
+    return adcLn,
+
+initFiles()
+ani=FuncAnimation(fig,update,10,None,blit=True,interval=10,repeat=True)
+plt.show()
+
