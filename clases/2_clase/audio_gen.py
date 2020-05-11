1e0c4604b37bf77c433e91fd51c879cb11e7462d
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2020-05-11 14:40:47 -0300

    corrijo clase2 y agrego doc de simpleaudio

--- clases/2_clase/audio_gen.py
+++ clases/2_clase/audio_gen.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 f   = 100
 fs  = 44100
 sec = 20
-B   = 5000
+B   = 1000
 t   = np.arange ( 0,sec,1/fs )
 note = (2**15-1)*np.sin(2 * np.pi * f * t) #sin
+#note = (2**15-1)*sc.sawtooth(2*np.pi*f*t,0) #saw
 #note = (2**15-1)*np.sin(2 * np.pi * B*t/sec * t)  #sweept
 
 audio = note.astype(np.int16)
